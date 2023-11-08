280ee5a912b3d61a0aeaecab46ee0e44276b8c30
Josemigg <soldado159753@gmail.com>
2023-11-08 14:42:27 -0400

    se llamo login desde authroutes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import morgan from 'morgan'
 import { fileURLToPath } from 'url'
+import { Login } from './routes/auth.routes.js'
 
 const app = express()
 app.use(morgan('dev'))
@@ -8,6 +9,6 @@ app.get('/', (req, res) => {
     res.sendFile('public/index.html', { root: fileURLToPath(import.meta.url).replace('app.js', '') })
 })
 
-app.get('/Login', Login)
+app.get('/login', Login)
 
 export default app
