8c51229afc7a23ad126fce3a24c19f68d724dd3e
Josemigg <soldado159753@gmail.com>
2023-11-08 14:11:15 -0400

    Created login function

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 import express from 'express'
 import morgan from 'morgan'
 import { fileURLToPath } from 'url'
+import {Login} from './routes/auth.routes'
 
 const app = express()
 app.use(morgan('dev'))
 app.get('/', (req, res) => {
     res.sendFile('public/index.html', { root: fileURLToPath(import.meta.url).replace('app.js', '') })
 })
+
+app.get('/Login', Login)
 
 export default app
