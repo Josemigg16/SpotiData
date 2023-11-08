f6b4d3cd040e534cc81e9dd0a92ddaf04f9b1c32
Christian Mendoza <christianmendozater@gmail.com>
2023-11-08 15:51:54 -0400

    Create scopes

--- src/config.js
+++ src/config.js
@@ -1,7 +1,15 @@
 import SpotifyWebApi from 'spotify-web-api-node';
 import { CLIENT_ID, SECRET_CLIENT } from './vars.js'
 
-var spotifyApi = new SpotifyWebApi({
+const scopes = [
+  'user-read-email',
+  'streaming',
+  'user-read-private',
+  'user-library-read',
+  "user-top-read"]
+
+
+const spotifyApi = new SpotifyWebApi({
   clientId: CLIENT_ID,
   clientSecret: SECRET_CLIENT,
   redirectUri: 'http://localhost:3000/callback'
