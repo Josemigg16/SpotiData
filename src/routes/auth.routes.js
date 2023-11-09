1aa0d618bafe1850de02f32eaffdc37ddd8d6de6
Christian Mendoza <christianmendozater@gmail.com>
2023-11-08 22:08:56 -0400

    create callback

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,15 +1,56 @@
 import jwt from 'jsonwebtoken'
 import { spotifyApi, scopes } from '../config.js'
 import { CLIENT_ID, SECRET_CLIENT } from '../vars.js';
+import session from 'express-session';
 
 export const Login = (req, res) => {
     res.redirect(spotifyApi.createAuthorizeURL(scopes));
 }
 
 export const callback = (req, res) => {
-    const error = req.query.error
-    const code = req.query.code
-    const state = req.query.state
+    const error = req.query.error;
+    const code = req.query.code;
+  
+    if (error) {
+      console.error('Callback Error:', error);
+      res.send(`Callback Error: ${error}`);
+      return;
+    }
+  
+    spotifyApi
+      .authorizationCodeGrant(code)
+      .then(data => {
+        const access_token = data.body['access_token'];
+        const refresh_token = data.body['refresh_token'];
+        const expires_in = data.body['expires_in'];
+  
+        spotifyApi.setAccessToken(access_token);
+        spotifyApi.setRefreshToken(refresh_token);
+        
+        console.log('access_token:', access_token);
+        console.log('refresh_token:', refresh_token);
 
+        req.session.token = access_token; 
+        console.log("KLKLKLKLKLKLKLKLKLKLKLKLKKLLK")
+        console.log(`${req.session.token}`);
+        console.log(
+          `Sucessfully retreived access token. Expires in ${expires_in} s.`
+        );
+        res.send('Success! You can now close the window.' );
+  
+        setInterval(async () => {
+          const data = await spotifyApi.refreshAccessToken();
+          const access_token = data.body['access_token'];
+  
+          console.log('The access token has been refreshed!');
+          console.log('access_token:', access_token);
+          spotifyApi.setAccessToken(access_token);
+        }, expires_in / 2 * 1000);
+      })
+      .catch(error => {
+        console.error('Error getting Tokens:', error);
+        res.send(`Error getting Tokens: ${error}`);
+      });
 
 }
+
