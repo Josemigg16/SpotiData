2c4cc3ab873017e8514a831703fa15c458512632
Christian Mendoza <christianmendozater@gmail.com>
2023-11-08 22:08:32 -0400

    create session

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import session from 'express-session';
 import morgan from 'morgan'
 import { fileURLToPath } from 'url'
 import { Login, callback } from './routes/auth.routes.js'
@@ -8,8 +9,14 @@ app.use(morgan('dev'))
 app.get('/', (req, res) => {
     res.sendFile('public/index.html', { root: fileURLToPath(import.meta.url).replace('app.js', '') })
 })
+app.use(session({
+    secret: 'yolavienuntaxi',
+    resave: false,
+    saveUninitialized: true
+  }));
 
 app.get('/login', Login)
 app.get('/callback', callback)
+
 
 export default app
