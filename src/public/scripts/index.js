4a5582e56e8b4f58c5eaa3278488d453709efe83
Christian Mendoza <christianmendozater@gmail.com>
2023-11-11 13:42:52 -0400

    spotify api methods,  get user data in index

---
+++ src/public/scripts/index.js
@@ -0,0 +1,31 @@
+import { Spotify } from "./spotify.js";
+const token = await getSession()
+
+const spotify = new Spotify(token);
+
+async function main() {
+  await spotify.getUserProfile()
+  console.log(spotify.userProfile)    
+}
+
+async function getSession() {
+  try {
+
+    const response = await fetch('/api/session');
+
+    if(!response.ok) {
+      throw new Error('No hay token'); 
+    }
+
+    const session = await response.json();
+
+    const { token } = session;
+    return token
+
+  } catch(error) {
+    console.error(error);
+  }
+
+}
+
+main()
--- src/public/scripts/script.js
+++
@@ -1,8 +0,0 @@
-//import {a} from './spotify';
-//console.log(a);
-fetch('/api/session')
-.then(res => res.json())
-.then(session => {
-  const { token } = session;
-  console.log(token);
-});
